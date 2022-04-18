180c0a8e7cf955a8ef883faf604192f4789b0bae
william <wang0930585791@gmail.com>
2022-04-18 18:41:57 +0800

    ADD JS FUNTION

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,14 @@
 document.getElementById("demo").innerHTML = 5 + 6;
+console.log("The answer should be 11.")
+
+var a = 0;
+function myFunction() {
+  if (a==0) {
+    document.getElementById("demo").style.fontSize = '35px';
+    a = 1;
+  }
+  else {
+    document.getElementById("demo").style.fontSize = '15px';
+    a = 0;
+  }
+}
